e5b144efa2ea41735d5fece1f256f8ceac05fdfb
unknown <Kutovoj.V@WS-6K-102-A07.ccam.cmc.unn.net>
2017-11-01 10:38:01 +0300

    Getting started

--- TStack/TStack.h
+++ TStack/TStack.h
@@ -1,18 +1,86 @@
+using namespace std;
+
 template <class T>
-class TStack{
+class TStack
+{
 	int MaxSize, Size;
 	T*Mas;
 public:
 	TStack(int _MaxSize = 100);
 	TStack(const TStack &T);
     ~TStack();
-
-	&TStack operator=(const TStack &T);
-
-	T Pop();
-	T Top();
-	void Push();
-
+	
 	int IsFull();
 	int IsEmpty();
+	
+	T Pop();
+	T Top();
+	void Push(T a);
 };
+    
+    template <class T>
+	TStack<T>::TStack(int _MaxSize)
+	{
+		if(MaxSize < 0) throw MaxSize;
+
+		MaxSize = _MaxSize;
+		Size = 0;
+		Mas = new T[MaxSize];
+	}
+
+    template <class T>
+	TStack<T>::TStack(const TStack &T)
+	{
+		MaxSize = T.MaxSize;
+		Size = T.Size;
+
+		for(int i = 0; i < Size; i++){
+			Mas[i] = T.Mas[i];
+	     }
+	}
+	
+	template <class T>
+	TStack<T>::~TStack()
+	{
+		delete[] Mas;
+	}
+    
+	template <class T>
+	int TStack<T>::IsFull()
+	{
+		return (MaxSize == Size);
+	}
+	
+	template <class T>
+	int TStack<T>::IsEmpty()
+	{
+		if(Size == 0) return 1;
+		else return 0;
+	}
+	
+	template <class T>
+	T TStack<T>::Pop() 
+	{
+		if(IsEmpty()) throw 0;
+		Size--;
+		
+		return Mas[Size];
+	}
+	
+	template <class T>
+	T TStack<T>::Top()
+	{
+		if(IsEmpty()) throw 0;
+
+		return Mas[Size];
+	}
+	
+	template <class T>
+	void TStack<T>::Push(T a)
+	{
+		if(IsFull()) throw Size;
+        else{
+			Mas[Size] = a;
+			Size++;
+		}
+	}
--- TestStack/test_TStack.cpp
+++ TestStack/test_TStack.cpp
@@ -4,6 +4,25 @@
 
 TEST(TStack, can_create_stack_with_positive_length)
 {
-  ASSERT_NO_THROW();
+	ASSERT_NO_THROW(TStack<int>(6));
 }
+
+TEST(TStack, throws_when_create_stack_with_negative_length)
+{
+	ASSERT_ANY_THROW(TStack<int>(-6));
+}
+
+TEST(TStack, can_create_copied_stack)
+{
+	TStack<int> St(5);
+	ASSERT_NO_THROW(TStack<int> St1(St));
+}
+
+TEST(TStack, throws_when_pop_empty_stack)
+{
+	TStack<int> St(0);
+	ASSERT_ANY_THROW(St.Pop());
+}
+
+
 
